9e4f5bcfd9d3fa14332976ba1ecca02ae8e96c89
BobvanVelzen <bob.vanvelzen@student.fontys.nl>
2019-02-14 14:07:13 +0100

    First commit

---
+++ Assets/Scripts/BigBomb.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigBomb : MonoBehaviour
+{
+    public float lifetime = 3f;
+    private float timeActive = 0f;
+    public float explosionRadius = 2f;
+    public float explosionForce = 10f;
+    public float explosionUpForce = 1f;
+
+    private bool exploded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeActive += Time.deltaTime;
+        if (timeActive >= lifetime && !exploded)
+            Explode();
+    }
+
+    private void Explode()
+    {
+        exploded = true;
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if (rb)
+            {
+                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius, explosionUpForce, ForceMode.Impulse);
+            }
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1f;
+
+    private Rigidbody rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0f, vertical);
+        if (movement != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + (movement * speed * Time.fixedDeltaTime));
+            transform.forward = movement;
+        }
+    }
+}
