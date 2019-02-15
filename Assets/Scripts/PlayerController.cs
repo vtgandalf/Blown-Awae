79a08f86db87b55be83dc89fba9fb471a12d9bd2
vtgandalf <v.v.todorow@gmail.com>
2019-02-15 12:54:09 +0100

    player movement restriction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public int rayLenght = 1;
     public float speed = 1f;
-
+    private Vector3 downVector;
+    private Vector3 position;
+    private Vector3 prevPosition;
     private Rigidbody rb;
-    
+    Collider childCollider;
+    public Collider groundCollider;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        childCollider = this.transform.GetChild(0).GetComponent<Collider>();
+        if (childCollider == null) Debug.Log("could not get collider");
     }
     
     void FixedUpdate()
     {
-        Move();
+        UpdateVectors();
+        if(CollisionWithTheGround()){
+            if(RayCastDown(rayLenght)) Move();
+            else{
+                transform.position = prevPosition;
+            }
+        }
     }
 
     void Move()
@@ -29,5 +41,38 @@ public class PlayerController : MonoBehaviour
             rb.MovePosition(rb.position + (movement * speed * Time.fixedDeltaTime));
             transform.forward = movement;
         }
+    }
+
+    bool RayCastDown(float rayLenght)
+    {
+        if(Physics.Raycast(transform.position, downVector, rayLenght)) 
+        {
+            prevPosition = transform.position;
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+
+    bool CollisionWithTheGround()
+    {
+        if (childCollider.bounds.Intersects(groundCollider.bounds))
+        {
+            Debug.Log("Over the ground");
+            return true;
+        }
+        else
+        {
+            Debug.Log("Not over the ground");
+            return false;
+        }
+    }
+
+    private void UpdateVectors()
+    {
+        position = transform.position;
+        downVector = this.transform.TransformDirection(Vector3.down);
     }
 }
