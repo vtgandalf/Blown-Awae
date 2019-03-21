067e0acd83b0c47b82c4d5d814d464b87a0876b2
Valeri Todorov <37157239+vtgandalf@users.noreply.github.com>
2019-03-21 09:50:33 +0100

    Power up icon indication
    + there is an icon indication of the bomb identifier that is going to be used next

---
+++ Assets/Scripts/PowerUpPlayerIndicator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerUpPlayerIndicator : MonoBehaviour
+{
+    public Player player;
+    private Image image;
+    // Update is called once per frame
+    void Awake() {
+        image = GetComponentInChildren<Image>();
+    }
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform.position);
+        if (player.bombEffect != null)
+        {
+            if(!image.gameObject.activeSelf) image.gameObject.SetActive(true);
+            image.sprite = player.bombEffect.image;
+        }
+        else
+        {
+            image.gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/Powerups/BombEffects/BombEffect.cs
+++ Assets/Scripts/Powerups/BombEffects/BombEffect.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BombEffect : ScriptableObject
 {
+    public Sprite image;
     public virtual void Activate(List<BombInteractable> hits) { }
 }
