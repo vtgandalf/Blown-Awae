f9df890056fd0aa8c2572f59a7ebcac6dacd561d
I346501 <bob.vanvelzen@student.fontys.nl>
2019-03-22 11:03:08 +0100

    DemoScript

---
+++ Assets/Scripts/DemoScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DemoScript : MonoBehaviour
+{
+    public KeyCode resetKey;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(resetKey))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
